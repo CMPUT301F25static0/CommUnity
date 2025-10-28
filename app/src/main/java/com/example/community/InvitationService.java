9d844c4b18851cb06b05cc9e77aeb6ebb1946aa2
Kristian Perez <kris.ilijay.perez@gmail.com>
2025-10-28 09:30:18 -0600

    feat: added InvitationService skeleton

---
+++ app/src/main/java/com/example/community/InvitationService.java
@@ -0,0 +1,33 @@
+package com.example.community;
+
+import java.util.List;
+import java.util.UUID;
+
+public class InvitationService {
+
+    InvitationService() {
+
+    }
+
+    public void createInvite() {
+
+    }
+
+    public void deleteInvite() {
+
+    }
+
+    public void updateInvite() {
+
+    }
+
+    public Invite getInviteByID(UUID inviteID){
+
+    }
+
+    public List<Invitation> getAllInvites() {
+
+    }
+
+    // probably should create helpers like getAllInvitesFromEvent
+}
