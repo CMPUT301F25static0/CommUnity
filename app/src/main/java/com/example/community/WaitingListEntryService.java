99cc94d281799b1b223b240b15e2c46e4d7a1dcb
Kristian Perez <kris.ilijay.perez@gmail.com>
2025-10-28 09:29:46 -0600

    feat: added WaitingListEntryService skeleton

---
+++ app/src/main/java/com/example/community/WaitingListEntryService.java
@@ -0,0 +1,32 @@
+package com.example.community;
+
+import java.util.List;
+import java.util.UUID;
+
+public class WaitingListEntryService {
+
+    WaitingListEntryService() {
+
+    }
+
+    public void createEntry() {
+
+    }
+
+    public void deleteEntry() {
+
+    }
+
+    public void updateEntry() {
+
+    }
+
+    public WaitingListEntry getENtryBYID(UUID entryID) {
+
+    }
+
+    public List<WaitingListEntry> getALLEntries() {
+
+    }
+
+}
