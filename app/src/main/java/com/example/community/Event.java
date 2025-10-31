b3acf100d552f21de02a2d1c0f8090d37a1a2de8
Fredrik Larida <fredrik.larida@gmail.com>
2025-10-31 02:13:19 -0600

    chore: Updated getter/setter, List type, added empty constructor

--- app/src/main/java/com/example/community/Event.java
+++ app/src/main/java/com/example/community/Event.java
@@ -1,9 +1,7 @@
 package com.example.community;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 public class Event {
     private String eventID; // need to make a UUID to string helper
@@ -21,10 +19,12 @@ public class Event {
 
     private String qrCodeImageURL;
 
-    private List<User> waitList;
-    private List<User> attendeeList;
-    private List<User> invitedList;
-    private List<User> cancelledList;
+    private List<String> waitListUserIDs;
+    private List<String> attendeeListUserIDs;
+    private List<String> invitedListUserIDs;
+    private List<String> cancelledListUserIDs;
+
+    public Event() { }
 
     public Event(String eventID, String title, String description, User organizer, String location,
                  String eventType, Integer capacity, LocalDateTime eventStartDate,
@@ -47,124 +47,112 @@ public class Event {
     public String getEventID() {
         return eventID;
     }
+    public void setEventID(String eventID) {
+        this.eventID = eventID;
+    }
 
     public String getTitle() {
         return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getDescription() {
         return description;
     }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public User getOrganizer() {
         return organizer;
+    }
+    public void setOrganizer(User organizer) {
+        this.organizer = organizer;
     }
 
     public String getLocation() {
         return location;
     }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
     public String getEventType() {
         return eventType;
+    }
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
     }
 
     public Integer getCapacity() {
         return capacity;
     }
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
 
     public LocalDateTime getEventStartDate() {
         return eventStartDate;
+    }
+    public void setEventStartDate(LocalDateTime eventStartDate) {
+        this.eventStartDate = eventStartDate;
     }
 
     public LocalDateTime getEventEndDate() {
         return eventEndDate;
     }
+    public void setEventEndDate(LocalDateTime eventEndDate) {
+        this.eventEndDate = eventEndDate;
+    }
 
     public LocalDateTime getRegistrationStart() {
         return registrationStart;
+    }
+    public void setRegistrationStart(LocalDateTime registrationStart) {
+        this.registrationStart = registrationStart;
     }
 
     public LocalDateTime getRegistrationEnd() {
         return registrationEnd;
     }
-
-    public String getQrCodeImageURL() {
-        return qrCodeImageURL;
-    }
-
-    public List<User> getWaitList() {
-        return waitList;
-    }
-
-    public List<User> getAttendeeList() {
-        return attendeeList;
-    }
-
-    public List<User> getInvitedList() {
-        return invitedList;
-    }
-
-    public List<User> getCancelledList() {
-        return cancelledList;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setOrganizer(User organizer) {
-        this.organizer = organizer;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public void setEventType(String eventType) {
-        this.eventType = eventType;
-    }
-
-    public void setCapacity(Integer capacity) {
-        this.capacity = capacity;
-    }
-
-    public void setEventStartDate(LocalDateTime eventStartDate) {
-        this.eventStartDate = eventStartDate;
-    }
-
-    public void setEventEndDate(LocalDateTime eventEndDate) {
-        this.eventEndDate = eventEndDate;
-    }
-
-    public void setRegistrationStart(LocalDateTime registrationStart) {
-        this.registrationStart = registrationStart;
-    }
-
     public void setRegistrationEnd(LocalDateTime registrationEnd) {
         this.registrationEnd = registrationEnd;
     }
 
+    public String getQrCodeImageURL() {
+        return qrCodeImageURL;
+    }
     public void setQrCodeImageURL(String qrCodeImageURL) {
         this.qrCodeImageURL = qrCodeImageURL;
     }
 
-    public void setWaitList(List<User> waitList) {
-        this.waitList = waitList;
+    public List<String> getWaitListUserIDs() {
+        return waitListUserIDs;
+    }
+    public void setWaitListUserIDs(List<String> waitListUserIDs) {
+        this.waitListUserIDs = waitListUserIDs;
     }
 
-    public void setAttendeeList(List<User> attendeeList) {
-        this.attendeeList = attendeeList;
+    public List<String> getAttendeeListUserIDs() {
+        return attendeeListUserIDs;
+    }
+    public void setAttendeeListUserIDs(List<String> attendeeListUserIDs) {
+        this.attendeeListUserIDs = attendeeListUserIDs;
     }
 
-    public void setInvitedList(List<User> invitedList) {
-        this.invitedList = invitedList;
+    public List<String> getInvitedListUserIDs() {
+        return invitedListUserIDs;
+    }
+    public void setInvitedListUserIDs(List<String> invitedListUserIDs) {
+        this.invitedListUserIDs = invitedListUserIDs;
     }
 
-    public void setCancelledList(List<User> cancelledList) {
-        this.cancelledList = cancelledList;
+    public List<String> getCancelledListUserIDs() {
+        return cancelledListUserIDs;
+    }
+    public void setCancelledListUserIDs(List<String> cancelledListUserIDs) {
+        this.cancelledListUserIDs = cancelledListUserIDs;
     }
 }
