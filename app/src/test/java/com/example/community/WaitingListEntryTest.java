2d498d834043871ee119d395c95e78bc81fecc86
Fredrik Larida <fredrik.larida@gmail.com>
2025-10-31 23:47:38 -0600

    Initial commit

---
+++ app/src/test/java/com/example/community/WaitingListEntryTest.java
@@ -0,0 +1,17 @@
+package com.example.community;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.sql.Timestamp;
+
+public class WaitingListEntryTest {
+
+    }
+}
+
