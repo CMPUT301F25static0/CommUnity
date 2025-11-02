62372c8fc70d78fb959eb8eb8f657d51083dba94
Fredrik Larida <fredrik.larida@gmail.com>
2025-11-02 00:25:28 -0600

    chore: implemented creating of document and reading document by UserID

--- app/src/main/java/com/example/community/UserRepository.java
+++ app/src/main/java/com/example/community/UserRepository.java
@@ -1,4 +1,33 @@
 package com.example.community;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class UserRepository {
+
+    private FirebaseFirestore db;
+    private CollectionReference usersRef;
+
+    public UserRepository() {
+        this.db = FirebaseFirestore.getInstance();
+        this.usersRef = db.collection("users");
+    }
+
+    public Task<Void> addUserToDatabase(User user) {
+        return usersRef
+            .document(user.getUserID())
+            .set(user);
+    }
+
+    public Task<DocumentSnapshot> getUserByUserId(String userID) {
+        return usersRef
+            .document(userID)
+            .get();
+    }
+
+    //TODO: Implement UPDATE Methods
+
+    //TODO: Implement DELETE Methods
 }
