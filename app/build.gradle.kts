1ef33646fd897aba6e2e45ac42223281e26496ca
taom06 <144624175+taom06@users.noreply.github.com>
2025-11-02 20:15:09 -0700

    Admin screen update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
