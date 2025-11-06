0560d033ecf4b7df958d22a22f1ada882bc01f22
Fredrik Larida <fredrik.larida@gmail.com>
2025-11-06 01:56:01 -0700

    Made enum

--- app/src/main/java/com/example/community/NotificationType.java
+++ app/src/main/java/com/example/community/NotificationType.java
@@ -1,9 +1,9 @@
 package com.example.community;
 
 public enum NotificationType {
-    WON,
-    LOST,
-    CANCELLED,
-    WAITLIST,
+    WON_LOTTERY,
+    LOST_LOTTERY,
+    CANCELLED_INVITE,
+    WAITLIST_UPDATE,
     INFO
 }
