a61408d682f9ec93a8e63553a4fd1d1326117a4d
Fredrik Larida <fredrik.larida@gmail.com>
2025-11-07 00:15:48 -0700

    Implemented Entrant dashboard

--- app/src/main/java/com/example/community/ArrayAdapters/EventArrayAdapter.java
+++ app/src/main/java/com/example/community/ArrayAdapters/EventArrayAdapter.java
@@ -1,22 +1,56 @@
 package com.example.community.ArrayAdapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.community.Event;
+import com.example.community.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class EventArrayAdapter extends ArrayAdapter<Event> {
-    public EventArrayAdapter(@NonNull Context context, int resource, @NonNull List<Event> objects) {
-        super(context, resource, objects);
+public class EventArrayAdapter extends RecyclerView.Adapter<EventArrayAdapter.ViewHolder> {
+    private List<Event> events;
+    public static TextView eventName;
+    public static TextView eventDescription;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View view) {
+            super(view);
+            eventName = view.findViewById(R.id.event_name);
+            eventDescription = view.findViewById(R.id.event_description);
+
+        }
     }
 
-    @NonNull
+    public EventArrayAdapter(List<Event> events) {
+        this.events = events;
+    }
+
     @Override
-    public View getView(int position, @Nullable View )
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.entrant_event_list_content, viewGroup, false);
+        return new ViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        Event event = events.get(position);
+        eventName.setText(event.getTitle());
+        eventDescription.setText(event.getDescription());
+    }
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
 }
+
+
