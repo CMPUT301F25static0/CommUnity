1ccaf708cadea789fb513fea48f6d0b534d8f913
Fredrik Larida <fredrik.larida@gmail.com>
2025-11-25 22:09:02 -0700

    Initial commit

---
+++ app/src/main/java/com/example/community/Screens/OrganizerScreens/OrganizerCreateNotificationFragment.java
@@ -0,0 +1,25 @@
+package com.example.community.Screens.OrganizerScreens;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.community.R;
+
+public class OrganizerCreateNotificationFragment extends Fragment {
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.organizer_create_notification_page, container, false);
+    }
+}
